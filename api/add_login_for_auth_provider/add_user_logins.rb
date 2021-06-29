4dea03cf4dcd6ddf7ad189ce115a767ff4f9c9f4
snelson82 <10576236+snelson82@users.noreply.github.com>
2021-06-29 17:08:46 -0600

    updated add_login_for_auth_provider + sets required CSV headers depending on script prompt answers + API call will include authentication_provider_id and/or sis_user_id depending on script prompt answers

--- api/add_login_for_auth_provider/add_user_logins.rb
+++ api/add_login_for_auth_provider/add_user_logins.rb
@@ -1,8 +1,10 @@
 # Working as of 01/27/2021
 require 'csv'
 require 'json'
+require 'byebug'
 require 'typhoeus'
 require 'io/console'
+require_relative '../../lib/progressbar'
 
 ### Prompts to set ENV, DOMAIN, TOKEN, and CSV FILE PATH
 
@@ -13,60 +15,137 @@ env != '' ? env << '.' : env
 
 ## DOMAIN
 puts 'Enter the domain, EX: <domain>.instructure.com'
-domain = gets.chomp!.downcase
+DOMAIN = gets.chomp!.downcase
+# DOMAIN = ''
 
 ## TOKEN
 puts 'Enter a valid access token to perform the API calls within this script'
-token = $stdin.noecho(&:gets).chomp!
+TOKEN = $stdin.noecho(&:gets).chomp!
+# TOKEN = ''
 
-## CSV FILE
-puts 'Enter the full file path for CSV data. EX: /Users/person/file/to/path.csv'
-csv_file = gets.chomp!
+################
+### CSV FILE ###
+################
 
-## AUTHENTICATION PROVIDER ID IN USE?
+puts 'Enter the full file path for CSV data. EX: /Users/person/path/to/file.csv'
+MAPPING_FILE = gets.chomp!
+# MAPPING_FILE = ''
+
+# Kill if file doesn't exist
+raise "CSV mapping file path is incomplete or file doesn't exist." unless File.exist?(MAPPING_FILE)
+
+# Gather header values
+required_headers = %w[canvas_user_id login_id] # fill this in with your headers with spaces escaped (This\ is\ one\ header)
+csv_headers = CSV.open(MAPPING_FILE, &:readline)
+
 puts 'Will you be setting the authentication_provider_id for the new logins? y/n'
-auth_pro_id = if gets.chomp!.upcase == 'Y'
+AUTH_PRO_ID = if gets.chomp!.upcase == 'Y'
                 'true'
               else
                 'false'
               end
 
-## SIS USER ID IN USE?
 puts 'Will you be assigning SIS IDs to the new logins? y/n'
-sis_id_used = if gets.chomp!.upcase == 'Y'
+SIS_ID_USED = if gets.chomp!.upcase == 'Y'
                 'true'
               else
                 'false'
               end
 
-base_url = "https://#{domain}.#{env}instructure.com/api/v1/accounts/self/logins/"
-default_headers = { Authorization: 'Bearer ' + token }
+required_headers << 'authentication_provider_id' if AUTH_PRO_ID == 'true'
+required_headers << 'sis_user_id' if SIS_ID_USED == 'true'
 
-CSV.foreach(csv_file, headers: true) do |row|
-  return raise "Invalid CSV headers: 'login_id' not found" if row['login_id'].nil?
-  return raise "Invalid CSV headers: 'canvas_user_id' not found" if row['canvas_user_id'].nil?
-  return raise "Invalid CSV headers: 'sis_user_id' not found" if row['sis_user_id'].nil? && sis_id_used == 'true'
-  return raise "Invalid CSV headers: 'authentication_provider_id' not found" if row['authentication_provider_id'].nil? && auth_pro_id == 'true'
+required_headers.each do |header|
+  raise "Invalid CSV headers: #{header} not found" unless csv_headers.include?(header)
+end
 
-  response = Typhoeus.post(
-    base_url,
-    headers: default_headers,
-    body: {
-      user: {
-        id: row['canvas_user_id'].to_i
-      },
-      login: {
-        unique_id: row['login_id'],
-        authentication_provider_id: row['authentication_provider_id'].to_s,
-        sis_user_id: row['sis_user_id'].to_s
-      }
-    }
-  )
-  # parse JSON data to save in readable array
-  data = JSON.parse(response.body)
-  if [200, 201].include?(response.code)
-    puts "#{row['canvas_user_id']} successfully updated"
-  else
-    puts "ERROR - HTTP Status #{response.code}. Login creation for user #{row['canvas_user_id']} failed"
+API_URL = "https://#{DOMAIN}.#{env}instructure.com/api/v1/accounts/self/logins/".freeze
+API_HEADERS = { authorization: "Bearer #{TOKEN}" }.freeze
+
+CSV.foreach(MAPPING_FILE, headers: true) do |row|
+  # Add additional data checks to skip the row if needed
+  @id        = row['canvas_user_id'].to_i
+  @unique_id = row['login_id']
+
+  request = if AUTH_PRO_ID == 'true' && SIS_ID_USED == 'true'
+              Typhoeus::Request.new(
+                API_URL,
+                method: :post,
+                headers: API_HEADERS,
+                body: {
+                  user: {
+                    id: @id
+                  },
+                  login: {
+                    unique_id: @unique_id,
+                    authentication_provider_id: row['authentication_provider_id'].to_s,
+                    sis_user_id: row['sis_user_id'].to_s
+                  }
+                }
+              )
+            elsif AUTH_PRO_ID == 'true' && SIS_ID_USED == 'false'
+              Typhoeus::Request.new(
+                API_URL,
+                method: :post,
+                headers: API_HEADERS,
+                body: {
+                  user: {
+                    id: @id
+                  },
+                  login: {
+                    unique_id: @unique_id,
+                    authentication_provider_id: row['authentication_provider_id'].to_s
+                  }
+                }
+              )
+            elsif AUTH_PRO_ID == 'false' && SIS_ID_USED == 'true'
+              Typhoeus::Request.new(
+                API_URL,
+                method: :post,
+                headers: API_HEADERS,
+                body: {
+                  user: {
+                    id: @id
+                  },
+                  login: {
+                    unique_id: @unique_id,
+                    sis_user_id: row['sis_user_id'].to_s
+                  }
+                }
+              )
+            elsif AUTH_PRO_ID == 'false' && SIS_ID_USED == 'false'
+              Typhoeus::Request.new(
+                API_URL,
+                method: :post,
+                headers: API_HEADERS,
+                body: {
+                  user: {
+                    id: @id
+                  },
+                  login: {
+                    unique_id: @unique_id
+                  }
+                }
+              )
+            else
+              raise 'Error: AUTH_PRO_ID and/or SIS_ID_USED are not correctly being set to true/false'
+            end
+  request.on_complete do |response|
+    data = JSON.parse(response.body)
+    # Use the next line to output any important value in testing or validation
+
+    # Typhoeus workflow to handle responses
+    if response.success?
+      puts "The user with the canvas_id of #{row['canvas_user_id']} was updated successfully"
+    elsif response.timed_out?
+      puts "ERROR (#{response.code}) - Request timed out"
+    elsif response.code.zero?
+      puts "ERROR (#{response.code}) - #{response.return_message}"
+    else
+      puts "ERROR (#{response.code}) - #{data['errors'].keys.first}: #{data['errors'][data['errors'].keys.first].first['message']}"
+    end
+    # Slight pause to allow API resources to recharge
+    sleep(0.2) if response.headers['X-Rate-Limit-Remaining'].to_i <= 200
   end
+  request.run
 end
